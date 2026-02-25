bc9a635d73f6162ad38a28c3f4881a043fe6c32b
Leticia-SS <leticia.saraiva013@gmail.com>
2026-02-24 23:12:23 -0300

    creating database with H2

--- src/main/java/br/edu/infnet/mercadorestapi/model/Produto.java
+++ src/main/java/br/edu/infnet/mercadorestapi/model/Produto.java
@@ -1,11 +1,18 @@
 package br.edu.infnet.mercadorestapi.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="produto")
 public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String nome;
     private int quantidade;
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO produto (nome, quantidade, preco) VALUES ("leite", 21, 10);
+INSERT INTO produto (nome, quantidade, preco) VALUES ("arroz", 65, 5);
+INSERT INTO produto (nome, quantidade, preco) VALUES ("carne", 56, 50);
+INSERT INTO produto (nome, quantidade, preco) VALUES ("mate", 84, 7);
