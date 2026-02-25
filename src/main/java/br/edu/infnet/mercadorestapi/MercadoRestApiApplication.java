47cc08898af21295a2d966c609d7580a48ea9271
Leticia-SS <leticia.saraiva013@gmail.com>
2026-02-24 21:53:54 -0300

    initial commit

---
+++ src/main/java/br/edu/infnet/mercadorestapi/MercadoRestApiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.mercadorestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadoRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadoRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/infnet/mercadorestapi/MercadoRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.mercadorestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
